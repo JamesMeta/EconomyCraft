0330ea59448b6c845bc1acfe0c8d3958c48fce7a
James Mata <jamesmata556@gmail.com>
2025-06-07 14:44:20 -0400

    share splitting is now a thing

---
+++ front-end/economycraft/lib/screens/holding/sell_holding_screen.dart
@@ -0,0 +1,18 @@
+import 'package:economycraft/classes/share.dart';
+import 'package:flutter/material.dart';
+
+class SellHoldingScreen extends StatefulWidget {
+  final Share share;
+
+  const SellHoldingScreen({super.key, required this.share});
+
+  @override
+  State<SellHoldingScreen> createState() => _SellHoldingScreenState();
+}
+
+class _SellHoldingScreenState extends State<SellHoldingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
